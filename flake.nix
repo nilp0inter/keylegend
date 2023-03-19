d3cddf5c5bd7a873aa2f95450047a2c34f75d0b5
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-03-19 12:12:44 +0100

    chore: flake environment

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell rec {
+
+      buildInputs = [
+        pkgs.pipenv
+        pkgs.zlib
+        pkgs.nodejs
+      ];
+
+      shellHook = ''
+        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+        export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+      '';
+
+    };
+  };
+}

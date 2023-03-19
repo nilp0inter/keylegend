76ef48291a436959499aa1f851b7577e408ae012
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-03-19 12:32:54 +0100

    feature: parametrize colors

--- keylegend.py
+++ keylegend.py
@@ -18,32 +18,32 @@ KEY_WIDTH = 100
 MARGIN = 5
 
 
-def create_keylegend(title, action, output_file, title_font_size, action_font_size, title_font_family, action_font_family):
+def create_keylegend(title, action, output_file, title_font_size, action_font_size, title_font_family, action_font_family, background_color, title_color, action_color):
     d = draw.Drawing(KEY_WIDTH, KEY_WIDTH, origin='center')
 
     # Draw a 1px black square to delimiter the perimeter
-    d.append(draw.Rectangle(-KEY_WIDTH/2, -KEY_WIDTH/2, KEY_WIDTH, KEY_WIDTH, fill='none', stroke='black', stroke_width=1))
+    d.append(draw.Rectangle(-KEY_WIDTH/2, -KEY_WIDTH/2, KEY_WIDTH, KEY_WIDTH, fill=background_color, stroke='black', stroke_width=1))
 
     # Draw the title text
     d.append(draw.Text(title, title_font_size, (-KEY_WIDTH/2) + MARGIN,
                        (-KEY_WIDTH/2) + title_font_size + MARGIN,
                        text_anchor='start', font_family=title_font_family,
-                       font_weight='bold'))
+                       font_weight='bold', fill=title_color))
 
     # Draw the action text
     if '$' not in action:
         d.append(draw.Text(action, action_font_size, 0, (title_font_size + 2*MARGIN) /
                            2, text_anchor='middle', font_family=action_font_family,
-                           font_variant="liga 0"))
+                           font_variant="liga 0", fill=action_color))
     else:
         # Action text are two lines. Centered taking into account the size.
         action1, action2, *_ = action.split('$')
         d.append(draw.Text(action1, action_font_size, 0, (title_font_size + 2*MARGIN) /
                            2 - action_font_size/2, text_anchor='middle', font_family=action_font_family,
-                           font_variant="liga 0"))
+                           font_variant="liga 0", fill=action_color))
         d.append(draw.Text(action2, action_font_size, 0, (title_font_size + 2*MARGIN) /
                            2 + action_font_size/2, text_anchor='middle', font_family=action_font_family,
-                           font_variant="liga 0"))
+                           font_variant="liga 0", fill=action_color))
 
     d.save_svg(output_file)
 
@@ -57,11 +57,14 @@ def main():
     parser.add_argument('--action-font-size', type=int, default=22, help='Font size for the action text. Default is 22.')
     parser.add_argument('--title-font-family', default='Atkinson Hyperlegible', help='Font family for the title text. Default is "Atkinson Hyperlegible".')
     parser.add_argument('--action-font-family', default='Atkinson Hyperlegible', help='Font family for the action text. Default is "Atkinson Hyperlegible".')
+    parser.add_argument('--background-color', default='none', help='Background color for the key legend. Default is "none".')
+    parser.add_argument('--title-color', default='black', help='Color for the title text. Default is "black".')
+    parser.add_argument('--action-color', default='black', help='Color for the action text. Default is "black".')
 
     args = parser.parse_args()
-
-    create_keylegend(args.title, args.action, args.output, args.title_font_size, args.action_font_size, args.title_font_family, args.action_font_family)
+    create_keylegend(args.title, args.action, args.output, args.title_font_size, args.action_font_size, args.title_font_family, args.action_font_family, args.background_color, args.title_color, args.action_color)
 
 
 if __name__ == "__main__":
     main()
+
